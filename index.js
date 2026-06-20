d4061a3e4943612975dd9089e85dc3276fded18c
Divya Sharma <sharmadivya2332@gmail.com>
2026-06-20 13:55:59 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import pizzaRouter from "./routers/pizza.router.js";
+import cartRouter from "./routers/cart.router.js";
+import userRouter from "./routers/user.router.js";
+import payRouter from "./routers/payment.router.js";
+import Razorpay from "razorpay";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+// const db = "mongodb+srv://divya1234:divya1234@cluster0.0k1wbyf.mongodb.net";
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true, // to allow cookies to be sent from frontend to backend
+  })
+);
+// app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const connectServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected to pizzamania!`);
+    app.listen(port, () => console.log(`Server on port: ${port}`));
+  } catch (error) {
+    console.log(`Error connecting to database: ${error}`);
+  }
+};
+connectServer();
+
+// mongoose
+//   .connect(`${db}/pizzamania`)
+//   .then(() => console.log(`Connected to pizzamania!`))
+//   .catch(() => console.log(`Error connecting to database`));
+
+app.use("/pizza-mania", pizzaRouter);
+app.use("/pizza-mania", cartRouter);
+app.use("/pizza-mania", userRouter);
+app.use("/pizza-mania", payRouter);
+
+export const instance = new Razorpay({
+  key_id: process.env.KEY_ID,
+  key_secret: process.env.KEY_SECRET,
+});
---
+++ routers/pizza.router.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  addPizza,
+  getPizza,
+  updatePizza,
+} from "../controllers/pizza.controller.js";
+
+const pizzaRouter = express.Router();
+
+pizzaRouter.post("/add-pizza", addPizza);
+pizzaRouter.get("/get-pizza", getPizza);
+pizzaRouter.patch("/update-pizza/:id", updatePizza);
+
+export default pizzaRouter;
